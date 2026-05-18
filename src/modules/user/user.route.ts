096cc42aa08d17f65fd34a6b916bc55001bb29de
RashidMahmud <rashidmahmud999@gmail.com>
2026-05-18 23:30:48 +0600

    feat: implement user creation endpoint with error handling

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,6 +1,31 @@
-import { Router } from "express";
+import { Router, type Request, type Response } from "express";
+import { pool } from "../../app";
 
 const router = Router();
 
+router.post("/api/users", async (req: Request, res: Response) => {
+  const { name, email, password, age } = req.body;
+
+  try {
+    const result = await pool.query(
+      `
+     INSERT INTO users(name,email,password,age) VALUES($1,$2,$3,$4) RETURNING *
+    `,
+      [name, email, password, age],
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "User Created successfully!",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+});
 
 export const userRoute = router
