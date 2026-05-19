f3328b22e60f3a1716c22d35e9ebd86b3855fe12
RashidMahmud <rashidmahmud999@gmail.com>
2026-05-19 21:15:18 +0600

    implement createProfile functionality and integrate it into the profile route

---
+++ src/modules/profile/profile-controller.ts
@@ -0,0 +1,10 @@
+import { type Response, type Request } from 'express';
+
+
+const createProfile = async (req: Request, res: Response) => {
+    
+};
+
+export const profileController = {
+  createProfile,
+};
