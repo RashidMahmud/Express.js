3c41280f0423b97bed4cceff1a55a10689da6f9b
RashidMahmud <rashidmahmud999@gmail.com>
2026-05-20 00:14:32 +0600

    feat: implement initial structure for createProfile function in profile controller

--- src/modules/profile/profile-controller.ts
+++ src/modules/profile/profile-controller.ts
@@ -1,8 +1,11 @@
-import { type Response, type Request } from 'express';
-
+import { type Response, type Request } from "express";
 
 const createProfile = async (req: Request, res: Response) => {
+  try {
+
+  } catch (error: any) {
     
+  }
 };
 
 export const profileController = {
