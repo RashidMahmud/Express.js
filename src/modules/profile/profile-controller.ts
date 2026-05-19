7981b1786001d4002246cb3f9c3312552c46b3a3
RashidMahmud <rashidmahmud999@gmail.com>
2026-05-20 00:48:09 +0600

    feat: implement createProfile function and enhance profileService for database integration

--- src/modules/profile/profile-controller.ts
+++ src/modules/profile/profile-controller.ts
@@ -1,11 +1,15 @@
 import { type Response, type Request } from "express";
+import { profileService } from "./profile-service";
 
 const createProfile = async (req: Request, res: Response) => {
   try {
-
-  } catch (error: any) {
-    
-  }
+    const result = await profileService.createProfileIntoDB(req.body);
+    res.status(201).json({
+      success: true,
+      message: "Profile created successfully!",
+      data: result.rows[0],
+    });
+  } catch (error: any) {}
 };
 
 export const profileController = {
--- src/modules/profile/profile-service.ts
+++ src/modules/profile/profile-service.ts
@@ -6,8 +6,20 @@ const createProfileIntoDB = async (payLoad: any) => {
 
   const user = await pool.query(
     `
-    SELECT * FROM users WHERE id = $1`, [user_id],
+    SELECT * FROM users WHERE id = $1`,
+    [user_id],
   );
+  if (user.rows.length === 0) {
+    throw new Error("user not exists!");
+  }
+
+  const result = await pool.query(
+    `
+    INSERT INTO profiles(user_id, bio, address, phone, gender) VALUES($1,$2,$3,$4,$5)
+    RETURNING *`,
+    [user_id, bio, address, phone, gender],
+  );
+  return result;
 };
 
 export const profileService = {
